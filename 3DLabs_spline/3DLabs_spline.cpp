83f3303ff0ec65ccf4ae00bbdb25b98d1a23f6bf
Александр Алексеевич Сподарев <aleksandr.alekseevich095@outlook.com>
2020-12-09 15:54:42 +0300

    первый коммит

--- 3DLabs_spline/3DLabs_spline.cpp
+++ 3DLabs_spline/3DLabs_spline.cpp
@@ -1,20 +1,72 @@
 ﻿// 3DLabs_spline.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
 
 #include <iostream>
+#include <fstream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+class Spline {
+	
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+	private: 
+		double(*in_points)[2];
+		double(*out_points)[2];
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+	public:
+
+		void set_in_points(double (*in_points)[2]) 
+		{
+				
+		}
+
+		double* get_in_points() 
+		{
+			return *in_points;
+		}
+
+		void set_out_points(double(*in_points)[2])
+		{
+
+		}
+
+		double* get_out_points() 
+		{
+			return *out_points;
+		}
+
+		void readFile() 
+		{
+			std::fstream in;
+
+			std::cout << "Чтение окончено" << std::endl;
+		}
+
+		void writeFile(double (*out_points)[2], int rowsCount) 
+		{
+			std::ofstream out;
+			out.open("C://Users/aspodarev/Desktop/3Dlabs/points.txt");
+			if (out.is_open())
+			{
+				int columnsCount = sizeof(*out_points) / sizeof(*out_points[0]);
+				for (int i = 0; i < rowsCount; i++)
+				{
+					for (int j = 0; j < columnsCount; j++)
+					{
+						out << out_points[i][j] << "\t";
+					}
+					std::cout << std::endl;
+				}
+			}
+			out.close();
+		}
+	
+
+};
+
+	int main()
+	{
+
+
+		return 0;
+	};
+
+
